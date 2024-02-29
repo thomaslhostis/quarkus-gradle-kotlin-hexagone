3d563250c396c1ed36e109c7d34e564c6298feef
Thomas L'Hostis <thomas.lhostis@gmail.com>
2024-02-29 12:21:41 +0100

    Nettoyage éventuel

--- core/presentation/build.gradle.kts
+++ core/presentation/build.gradle.kts
@@ -9,7 +9,9 @@ val quarkusVersion: String by project
 
 dependencies {
     implementation(project(":core:application"))
+
+//    implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:${quarkusVersion}"))
+
     implementation("io.quarkus:quarkus-resteasy-reactive-jackson:${quarkusVersion}")
     implementation("io.quarkus:quarkus-resteasy-reactive:${quarkusVersion}")
-    implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:${quarkusVersion}"))
 }
